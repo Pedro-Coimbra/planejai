5282da0745cb05c0d6198b889242119f16d89d1e
Pedro Santos <pedro.hcoimbra97@gmail.com>
2026-09-02 11:17:00 -0300

    Finalizando a página de resultados da simulação

--- src/hooks/useSimulationStorage.tsx
+++ src/hooks/useSimulationStorage.tsx
@@ -21,6 +21,7 @@ export const useSimulationStorage = () => {
   }
 
   const getFormData = (id: string): SimulationRecord | null => {
+    console.log('id', id)
     const storage = localStorage.getItem(LOCAL_STORAGE_KEY)
     if (!storage) {
       return null
@@ -30,5 +31,16 @@ export const useSimulationStorage = () => {
     return savedData.find((record) => record.id === id) || null
   }
 
-  return { saveFormData, getFormData }
+  const updateSimulation = (id: string, data: SimulationRecord) => {
+    const storage = localStorage.getItem(LOCAL_STORAGE_KEY)
+    const savedData = storage ? (JSON.parse(storage) as SimulationRecord[]) : []
+
+    const updated = savedData.map((record) =>
+      record.id === id ? { ...data } : record,
+    )
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updated))
+  }
+
+  return { saveFormData, getFormData, updateSimulation }
 }
